81d501a861a8b142654cc5b176dac44afe765b8d
Tze Lin Ong <tezong77@yahoo.com>
2020-12-07 19:31:56 -0800

    initial attempt at MI

--- app.js
+++ app.js
@@ -7,7 +7,29 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+
+// Load .env if it exists. 
+require("dotenv").config();
+
 var app = express();
+
+// DefaultAzureCredential expects AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET to be loaded as env vars
+// or else use ManagedIdentityCredential
+const credential = new DefaultAzureCredential();
+const url = process.env["KEYVAULT_URI"] || "<keyvault-uri>";
+const client = new SecretClient(url, credential);
+
+// get a secret
+const getWebChatSecret = async() => {
+  const sec = await client.getSecret('WebChatSecretKey');
+  console.log("***SECRET: ", sec.value);
+
+  return sec;
+}
+
+const webChatSecret = getWebChatSecret();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
